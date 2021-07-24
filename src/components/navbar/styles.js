74b4c1ec71eb93d8afc65b69146066fcb3cbdbb8
Daniel Santos <Daniel Santos>
2021-07-24 14:30:06 -0300

    first commit

---
+++ src/components/navbar/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const NavbarWrapper = styled.ul``;
+
+export const NavbarItem = styled.li``;
+
+export const ItemText = styled.a``;
---
+++ src/components/shared.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Title = styled.h1``;
+
+export const SubTitle = styled.h2``;
+
+export const Main = styled.main``;
